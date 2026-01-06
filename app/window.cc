dcbaed96beedb185d37832380656daf593023d8c
makifcevik <makif.cevik02@gmail.com>
2026-01-06 15:16:13 +0300

    Implement move operations in `Window` class.

--- app/window.cc
+++ app/window.cc
@@ -2,12 +2,14 @@
 
 #include "window.h"
 
+#include <utility>
+
 #include <fmt/core.h>
 
 // Set `ok_` flag true if window creation is successfull, false otherwise
 Window::Window(const Config& config) {
-  window_ = SDL_CreateWindow(config.title.c_str(), config.x, config.y, config.width,
-                             config.height, config.flags);
+  window_ = SDL_CreateWindow(config.title.c_str(), config.x, config.y,
+                             config.width, config.height, config.flags);
   if (!window_) {
     fmt::println(stderr, "Error creating SDL Window: {}", SDL_GetError());
     ok_ = false;
@@ -15,6 +17,25 @@ Window::Window(const Config& config) {
     ok_ = true;
   }
 };
+
+// Move constructor
+Window::Window(Window&& other) noexcept
+    : window_(std::exchange(other.window_, nullptr)),
+      ok_(std::exchange(other.ok_, false)) {}
+
+// Move assignment
+Window& Window::operator=(Window&& other) noexcept {
+  if (this != &other) {
+    // Destroy current window before taking the new one
+    if (window_) {
+      SDL_DestroyWindow(window_);
+    }
+    window_ = std::exchange(other.window_, nullptr);
+    ok_ = std::exchange(other.ok_, false);
+  }
+  return *this;
+}
+
 // Destroy only if window is valid
 Window::~Window() noexcept {
   if (ok_) {
