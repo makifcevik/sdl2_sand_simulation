35bf7102fce9205262b4aeb8e3d559d7517872bf
makifcevik <makif.cevik02@gmail.com>
2026-01-08 08:07:14 +0300

    Refactor main loop. Use new App class.

--- app/main.cc
+++ app/main.cc
@@ -1,36 +1,9 @@
 // MIT License
 
-#include <SDL.h>
-#include <fmt/core.h>
-
-#include "window.h"
-#include "renderer.h"
-#include "input.h"
+#include "app.h"
 
 int main(int argc, char* argv[]) {
-  fmt::print("Hello, Professional C++ World!\n");
-
-  if (SDL_Init(SDL_INIT_VIDEO) < 0) {
-    fmt::println(stderr, "Error initializing SDL: {}", SDL_GetError());
-  }
-
-  Window window;
-  if (!window.Get())
-    return 1;
-
-  Renderer renderer(window);
-
-  Input input;
-
-  bool is_running = true;
-  SDL_Event event;
-  while (is_running) {
-    while (SDL_PollEvent(&event)) {
-      input.BeginFrame();
-      input.ProcessEvent(event);
-      is_running = !input.QuitRequested();
-    }
-  }
-
-  return 0;
+  // Run App with default configuration
+  App app;
+  return app.Run();
 }
