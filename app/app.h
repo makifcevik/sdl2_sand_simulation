30467b7287ee3cb9a55ccfc6e7bd477bcf41e8b6
makifcevik <makif.cevik02@gmail.com>
2026-01-08 08:06:12 +0300

    Introduce App class which owns systems and manages the game loop.

---
+++ app/app.h
@@ -0,0 +1,53 @@
+// MIT License
+
+#ifndef SDL2_SAND_SIMULATION_APP_APP_H_
+#define SDL2_SAND_SIMULATION_APP_APP_H_
+
+#include <memory>
+
+#include "input.h"
+#include "renderer.h"
+#include "window.h"
+
+// This class acts like the "Director"
+// It owns `Window`, `Renderer`, and `Input` systems and
+// manages the Game Loop
+class App {
+ public:
+  struct Config {
+    Window::Config window_config;
+    Renderer::Config renderer_config;
+  };
+
+  // Constructor with default configuration values
+  App() : App(Config{}) {}
+
+  explicit App(const Config&);
+  ~App() noexcept;
+
+  // Disallow copies
+  App(const App&) = delete;
+  App& operator=(const App&) = delete;
+
+  // Disallow moves (App is the ROOT object, moving it risks killing SDL)
+  App(App&&) = delete;
+  App& operator=(App&&) = delete;
+
+  // The main entry point. Returns the exit code (0 for success).
+  int Run();
+
+ private:
+  // The 3 phases of the game loop
+  void PollEvents();
+  void Update();
+  void Render();
+
+  // Systems (order matters! Window must be created before the Renderer).
+  std::unique_ptr<Window> window_;
+  std::unique_ptr<Renderer> renderer_;
+  std::unique_ptr<Input> input_;
+
+  bool is_running_{false};
+};
+
+#endif  // SDL2_SAND_SIMULATION_APP_APP_H_
