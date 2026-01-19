8c69a3232a336b5ac2c661e438a749b60537fec7
makifcevik <makif.cevik02@gmail.com>
2026-01-19 22:05:18 +0300

    Introduced `world.h` defining the World class, which manages a 2D grid of cells for sand simulation.

---
+++ core/include/world.h
@@ -0,0 +1,46 @@
+// MIT License
+
+#ifndef SDL2_SAND_SIMULATION_CORE_SRC_WORLD_H_
+#define SDL2_SAND_SIMULATION_CORE_SRC_WORLD_H_
+
+#include <cstdint>
+
+#include <vector>
+
+// Defines the main world.
+// Simulation is calculated in this class.
+// Set the simulation width and height in the constructor.
+// Use `GetCells()` to retrieve all the underlying cells.
+// Call the `Update()` method to run the simulation.
+class World {
+ public:
+  enum class CellType : uint8_t { kEmpty = 0, kSand = 1 };
+
+  // Set the simulation width and height.
+  World(int32_t width, int32_t height);
+
+  // Handles its own internal logic.
+  // Pass the frame_count (required for randomness)
+  void Update(uint32_t frame_count);
+
+  // Internally checks if coordinates are valid
+  void SetCell(int32_t x, int32_t y, CellType type);
+
+  // Internally checks if coordinates are valid
+  CellType GetCell(int32_t x, int32_t y) const;
+
+  int32_t GetWidth() const { return width_; };
+  int32_t GetHeight() const { return height_; };
+
+  // You can use it to access the underlying cells (can be fed to a graphics API).
+  const std::vector<CellType>& GetCells() const { return cells_; };
+
+ private:
+  std::vector<CellType> cells_;
+  int32_t width_;
+  int32_t height_;
+
+  bool IsValid(int32_t x, int32_t y) const;
+};
+
+#endif  // SDL2_SAND_SIMULATION_CORE_SRC_WORLD_H_
